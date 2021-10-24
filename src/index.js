9a54f719e1f1d51d92f20cb7c30a8b6fa7c3d863
Xyle Nicole Puzon Peralta <38389266+xylePeralta@users.noreply.github.com>
2021-10-24 16:54:40 -0600

    Made a list altered template to make a list using react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
+  <div>
+    <h1>Here is a list</h1>
+    <ul>
+      <li>Something that goes first</li>
+      <li>Something that goes second</li>
+      <li>Something that goes third</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
 );
