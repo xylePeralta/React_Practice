1aabbc2db1b9d14534156c3f9fc74656c94778a1
Xyle Nicole Puzon Peralta <38389266+xylePeralta@users.noreply.github.com>
2021-10-24 17:19:42 -0600

    JSX expression learning to use variables with JSX.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const firstName = "Nikki";
+const lastName = "Peralta";
+var pokemon1 = "bulbasaur";
+var pokemon2 = "cyndaquil";
+var pokemon3 = "treecko";
+
 ReactDOM.render(
   <div>
-    <h1>Here is a list</h1>
+    <h1>
+      Here are {firstName} {lastName}'s favourite starter pokemon
+    </h1>
     <ul>
-      <li>Something that goes first</li>
-      <li>Something that goes second</li>
-      <li>Something that goes third</li>
+      <li>{pokemon1}</li>
+      <li>{pokemon2}</li>
+      <li>{pokemon3}</li>
     </ul>
   </div>,
   document.getElementById("root")
